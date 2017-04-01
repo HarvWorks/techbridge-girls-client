93f48656d1888b623c602d4aa37c9ad3932f12ca
Philip Vo <philiptranbavo@gmail.com>
2017-03-31 21:32:11 -0700

    Updated component logic.

--- src/app/components/supplier/messages/messages.component.ts
+++ src/app/components/supplier/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { HttpService } from '../../../services/http.service';
 import { SessionService } from '../../../services/session.service';
 import { SocketService } from '../../../services/socket.service';
 
@@ -12,6 +13,7 @@ export class MessagesComponent implements OnInit, OnDestroy {
   private OnSent;
 
   constructor(
+    private http: HttpService,
     private router: Router,
     private session: SessionService,
     private socket: SocketService,
