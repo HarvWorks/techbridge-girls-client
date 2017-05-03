6249af2d63a0f4186df47390d5bbb8b3a9dae5f0
George Miranda <George Miranda>
2017-05-03 10:10:12 -0700

    styling

--- src/app/components/tech-bridge-girls/donate/donate.component.ts
+++ src/app/components/tech-bridge-girls/donate/donate.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
+import { HttpService } from '../../../services/http.service';
+
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html'
 })
-export class DonateComponent { }
+export class DonateComponent { 
+
+  constructor(
+    private http: HttpService
+  ) {
+
+  }
+
+  donate(): void {
+    console.log('pressed');
+    this.http.post('/donate', {amount: '10.00'})
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  }
+}
